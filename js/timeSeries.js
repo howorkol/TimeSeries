feade5cabe0ae46265202e20e319e85be1b3557a
Lee Howorko <howorkol@mymacewan.ca>
2014-09-19 19:04:33 -0600

    Change to light theme. Adjusted search box

--- js/timeSeries.js
+++ js/timeSeries.js
@@ -2,6 +2,6 @@ $('#accordion_cont').liteAccordion({
     containerWidth: $(window).width() - 23,
     containerHeight: $(window).height() - 28,
     slideSpeed: 1000,
-    theme : 'dark'
+    theme : 'light'
 });
 
