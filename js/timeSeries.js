93dcacdd130baa6f5149c0996f342967bb4b7e27
Lee Howorko <howorkol@mymacewan.ca>
2014-09-24 22:33:35 -0600

    Add chart adding/resizing. As charts are added, the others shrink.

--- js/timeSeries.js
+++ js/timeSeries.js
@@ -29,7 +29,7 @@ $('#searchBox > input#button').click(function () {
         disable_slides = false;
     
         $('.company_name').text(search_term);
-        $('#viz_slide').click();
+        $('ol li:nth-child(2) span').click();
 
         $('#searchBox > input#text').val('');
     });
@@ -45,6 +45,14 @@ $('div#compare_cont > div').click(function () {
     }
 });
 
+$('input#add').click(function() {
+    chart_count += 1;
+    chart_height = update_chart_height(chart_count);
+    add_attribute('new' + chart_count, chart_height, function() {
+        
+    });
+});
+
 function get_data(search_term, option, callback) {
     var chart_height = 0;
     
@@ -52,15 +60,16 @@ function get_data(search_term, option, callback) {
         company_list.push(search_term);
         
         if (chart_count === 0) {
-            attribute_list.push('initial attr');
+            var new_attr = 'open';
+            attribute_list.push(new_attr);
             
             // Get data from Quandl.
             chart_count += 1;
             chart_height = update_chart_height(chart_count);
-            add_attribute(chart_height, callback);
+            add_attribute(new_attr, chart_height, callback);
         } else {
             // Get data from Quandl.
-            add_company(callback);
+            add_company('Google', callback);
         }
     } else if (option === 'add_attribute') {
         attribute_list.push(search_term);
@@ -77,29 +86,49 @@ function get_data(search_term, option, callback) {
 
 function add_company(company, callback) {
     // Go through each chart and add this company to it.
+    console.log(chart_count);
 
     callback();
 }
 
 function add_attribute(attribute, chart_height, callback) {
     // Create a new chart and add each company to it.
+    var rect = d3.select('div#visualizations svg')
+        .append('rect')
+        .attr('class', 'chart')
+        .attr('id', attribute)
+        .attr('position', chart_count - 1)
+        .attr('width', $('div#visualizations').width())
+        .attr('height', chart_height)
+        .attr('stroke', 'black')
+        .attr('stroke-width', 1)
+        .attr('y', chart_height * (chart_count - 1))
+        .attr('fill', function() {
+            return 'rgb(0,0,' + ((chart_count - 1) * 10) + ')';
+        });
 
     callback();
 }
 
-
-
-
-
-
-
 function update_chart_height(chart_count) {
-    var chart_height = $('#viz div.viz_cont').height();
+    var total_height = $('div#visualizations').height();
     
-    chart_height = ((chart_height / chart_count) > 200)
-            ? chart_height
+    // Calculate the new chart height.
+    chart_height = ((total_height / chart_count) > 200)
+            ? total_height / chart_count
             : 200;
+    
     // Go through all existing charts and make them the new height.
+    // The y position needs to be updated as well.
+    d3.selectAll('rect.chart')
+        .attr('height', chart_height)
+        .attr('y', function() {
+            return $(this).attr('position') * chart_height;
+        });
+    
+    // Resize the svg element to fit everything.
+    d3.select('svg')
+        .attr('height', chart_height * chart_count);
     
     return chart_height;
 }
